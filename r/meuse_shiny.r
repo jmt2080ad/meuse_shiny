52dac09ecd6324683f52f06b710295a73632b4a4
jmt2080ad <jason.morgan.taylor@gmail.com>
2017-11-08 09:29:29 -0800

    add gitignore and test

--- r/meuse_shiny.r
+++ r/meuse_shiny.r
@@ -11,5 +11,5 @@ x <- readRDS("./data_output/surfaces.rds")
 plot(x$idw[[1]], 
      breaks = c(0,10,20,40), 
      col = terrain.colors(3),
-     main="Digital Surface Model (DSM)\n NEON Harvard Forest Field Site")
+     main="")
 
