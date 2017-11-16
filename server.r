f043e453854ba8b3f224a50dd30490b5b5e94c12
jmt2080ad <jason.morgan.taylor@gmail.com>
2017-11-16 05:38:28 -0800

    edit server.r

--- server.r
+++ server.r
@@ -40,28 +40,6 @@ idwFun <- function(valsSp, meuseGrid, nmin, nmax, maxdist, idp){
     )
 }
 
-## build shiny app
-ui <- fluidPage(
-    titlePanel("Meuse IDW with MAE (Mean Abolute Error)"),
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("SelAna",
-                        "Analyte",
-                        c("Cadmium" = "cadmium",
-                          "Copper"  = "copper",
-                          "Lead"    = "lead",
-                          "Zinc"    = "zinc")),
-            numericInput("nmin", "Minimum Points", 0),
-            numericInput("nmax", "Maximum Points", 5),
-            numericInput("maxdist", "Maximum Search Distance", 500),
-            numericInput("idp", "IDW Power", 2)
-            ),
-        mainPanel(
-            plotOutput(outputId = "rastPlot", height = "550px")
-            )
-        )
-    )
-
 server <- function(input, output){
     output$rastPlot <- renderPlot({
         validate(
